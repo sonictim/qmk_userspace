33f0f54778016070bf5cfb755d0ed35e14e24e59
sonictim <soundguru@gmail.com>
2026-09-23 01:00:00 -0500

    fixed up warning

--- keyboards/bastardkb/charybdis/4x6/keymaps/tjf/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/tjf/keymap.c
@@ -34,7 +34,7 @@ enum charybdis_keymap_layers {
 // #define CHARYBDIS_AUTO_SNIPING_ON_LAYER LAYER_POINTER
 
 #ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
-static uint16_t auto_pointer_layer_timer = 0;
+// static uint16_t auto_pointer_layer_timer = 0;
 
 #    ifndef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD
 #        define CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD 8
